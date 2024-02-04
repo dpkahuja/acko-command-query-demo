279929b0fa84c7aecd2e345930d06d750ae1886e
Aryan <aryan.garg@acko.tech>
2024-02-04 07:44:25 +0530

    added suggestions for configuring event handlers

--- src/main/java/com/acko/dynamicdatasourcerouting/events/employee/AuditEventEmployeeConfiguration.java
+++ src/main/java/com/acko/dynamicdatasourcerouting/events/employee/AuditEventEmployeeConfiguration.java
@@ -1,37 +1,29 @@
 package com.acko.dynamicdatasourcerouting.events.employee;
 
 import com.acko.dynamicdatasourcerouting.audit.AuditEventHandlerConfig;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-
+import com.acko.dynamicdatasourcerouting.events.employee.models.EmployeeCreated;
 import com.acko.dynamicdatasourcerouting.events.employee.models.EmployeeFound;
+import com.acko.dynamicdatasourcerouting.events.employee.models.LogCreated;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuditEventEmployeeConfiguration {
+
+  private final EmployeeCreatedNotificationHandler employeeCreatedNotificationHandler;
+  private final EmployeeFoundHandler employeeFound;
+  private final LogCreatedHandler logCreatedHandler;
+
+  /* Employee class can simply register its own handlers and create a bean for the config class
+  This bean would then be used by our dispatcher for dispatching events to the configured handler */
   @Bean
-  public AuditEventHandlerConfig auditEventHandlerConfig(
-      Map<String, List<EventHandler>> eventHandlers) {
-    AuditEventHandlerConfig config = new AuditEventHandlerConfig(eventHandlers);
+  public AuditEventHandlerConfig auditEventHandlerConfig() {
+    AuditEventHandlerConfig config = new AuditEventHandlerConfig();
+    config.registerHandler(EmployeeCreated.class, employeeCreatedNotificationHandler);
+    config.registerHandler(EmployeeFound.class, employeeFound);
+    config.registerHandler(LogCreated.class, logCreatedHandler);
     return config;
-  }
-
-  @Bean
-  public Map<String, List<EventHandler>> eventHandlers(
-      EmployeeCreatedNotificationHandler employeeCreatedNotificationHandler,
-      EmployeeFoundHandler employeeFound,
-      LogCreatedHandler logCreatedHandler) {
-    Map<String, List<EventHandler>> handlersMap = new HashMap<>();
-
-    handlersMap.put(
-        "EmployeeCreated",
-        Arrays.asList(employeeCreatedNotificationHandler));
-    handlersMap.put(
-            "EmployeeFound",
-            Arrays.asList(employeeFound));
-    handlersMap.put("LogCreated", Arrays.asList(logCreatedHandler));
-
-    return handlersMap;
   }
 }
